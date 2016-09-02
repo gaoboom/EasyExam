3e061e6e11211e6ffb56cd39d40b5bdebac71b5d
Peng Gao <txq_417@163.com>
2016-09-02 12:08:05 +0800

    New Created

---
+++ EasyExam/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EasyExam.Startup))]
+namespace EasyExam
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
