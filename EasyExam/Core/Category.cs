d7c19a4dec783259da07b0a2b5e6cc598adfeb70
Peng Gao <txq_417@163.com>
2016-09-02 17:38:22 +0800

    Some classes added

---
+++ EasyExam/Core/Category.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EasyExam.Core
+{
+    /// <summary>
+    /// 栏目模型类
+    /// </summary>
+    public class Category
+    {
+        [Key]
+        public int CategoryID { get; set; }
+
+        /// <summary>
+        /// 栏目名称
+        /// </summary>
+        [Required(ErrorMessage = "{0}必填")]
+        [StringLength(50, ErrorMessage = "不得多于{0}个字")]
+        [Display(Name = "栏目名称")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 父栏目ID
+        /// </summary>
+        [Required(ErrorMessage = "必填")]
+        [Display(Name = "父栏目")]
+        public int ParentID { get; set; }
+
+        /// <summary>
+        /// 栏目说明
+        /// </summary>
+        [DataType(DataType.MultilineText)]
+        [StringLength(1000, ErrorMessage = "不得多于{0}个字")]
+        [Display(Name = "栏目说明")]
+        public string Description { get; set; }
+
+        /// <summary>
+        /// 栏目顺序【同级栏目数字越小越靠前】
+        /// </summary>
+        [Display(Name = "栏目顺序")]
+        public int Order { get; set; }
+
+    }
+}
---
+++ EasyExam/Core/User.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EasyExam.Core
+{
+    /// <summary>
+    /// 用户模型
+    /// <remarks>Created at 2016.09.02</remarks>
+    /// </summary>
+    public class User
+    {
+        [Key]
+        public int UserID { get; set; }
+
+        /// <summary>
+        /// 登录名
+        /// </summary>
+        [StringLength(50, MinimumLength = 4, ErrorMessage = "{0}长度为{2}-{1}个字符")]
+        [Display(Name = "登录名")]
+        public string Username { get; set; }
+
+        /// <summary>
+        /// 登录密码
+        /// </summary>
+        [DataType(DataType.Password)]
+        [StringLength(256, ErrorMessage = "{0}长度少于{1}个字符")]
+        [Display(Name = "密码")]
+        public string Password { get; set; }
+
+        /// <summary>
+        /// 名称【可做昵称、真实姓名等】
+        /// </summary>
+        [StringLength(20, ErrorMessage = "{0}必须少于{1}个字符")]
+        [Display(Name = "名称")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 分公司
+        /// </summary>
+        [StringLength(20, ErrorMessage = "{0}必须少于{1}个字符")]
+        [Display(Name = "分公司")]
+        public string Company { get; set; }
+
+        /// <summary>
+        /// 所属部门
+        /// </summary>
+        [StringLength(20, ErrorMessage = "{0}必须少于{1}个字符")]
+        [Display(Name = "所属部门")]
+        public string Department { get; set; }
+
+        /// <summary>
+        /// 最后登录时间
+        /// </summary>
+        [DataType(DataType.DateTime)]
+        [Display(Name = "最后登录时间")]
+        public Nullable<DateTime> LastLoginTime { get; set; }
+
+        /// <summary>
+        /// 最后登录IP
+        /// </summary>
+        [Display(Name = "最后登录IP")]
+        public string LastLoginIP { get; set; }
+    }
+}
