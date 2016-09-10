155ce206b5811c5f7e4916e2a7c44ae58cf3032f
Ben <56520085@qq.com>
2016-09-10 17:52:25 +0800

    Test Class to be finished

---
+++ EasyExam/Core/Test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EasyExam.Core
+{
+    /// <summary>
+    /// 测验模型
+    /// </summary>
+    public class Test
+    {
+        [Key]
+        public int TestID { get; set; }
+
+        public string Name { get; set; }
+    }
+}
